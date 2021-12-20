1ea98d69fd9fc4bb5f830edc7d3a8e4d09791c41
Aaron Vazquez <aaron.vazquez.urquiza@gmail.com>
2021-12-20 11:09:47 -0600

    "Added more data types"

--- 02-01-TiposDatosJavaScript.js
+++ 02-01-TiposDatosJavaScript.js
@@ -1,3 +1,21 @@
+/* 
+   Ejemplos de
+   tipos de
+   datos
+*/
+
 //Tipos de dato String
-let name = Steve;
+var name = Steve;
 console.log(name);
+
+//Tipo de dato numerico
+var number = 7;
+console.log(number);
+
+//Tipo de dato object
+var objeto = {
+    name: "Steve",
+    last: "Rogers",
+    phone: "4721652234"
+}
+console.log(objeto)
