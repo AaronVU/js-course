d2331cb70aaaae3804ae221ca35d8d819a988872
Aaron Vazquez <aaron.vazquez.urquiza@gmail.com>
2022-01-02 13:16:43 -0600

    "Added second exercise"

---
+++ 02-02-TypeOfJavaScript.js
@@ -0,0 +1,15 @@
+//Tipo de dato String
+var name = Steve;
+console.log(typeof name);
+
+//Tipo de dato numerico
+var number = 7;
+console.log(typeof number);
+
+//Tipo de dato object
+var objeto = {
+    name: "Steve",
+    last: "Rogers",
+    phone: "4721652234"
+}
+console.log(typeof objeto)
