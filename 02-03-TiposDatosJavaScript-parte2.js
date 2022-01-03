cbf35cc1700602eb1cb8feefcd48455320013b9a
Aaron Vazquez <aaron.vazquez.urquiza@gmail.com>
2022-01-03 15:08:30 -0600

    Added the third exercise of the course

---
+++ 02-03-TiposDatosJavaScript-parte2.js
@@ -0,0 +1,44 @@
+//Tipo de dato String
+var name = Steve;
+console.log(typeof name);
+
+//Tipo de dato numerico
+var number = 7;
+console.log(typeof number);
+
+//Tipo de dato object
+var objeto = {
+    name: "Steve",
+    last: "Rogers",
+    phone: "4721652234"
+}
+console.log(typeof objeto);
+
+//Tipo de dato boolean (true, false)
+var flag = false;
+console.log(typeof flag);
+
+//Tipo de dato function
+function myFunction(){}
+console.log(typeof nyFunction);
+
+//Tipo de dato Symbol
+var simbolo = Symbol("My symbol");
+console.log(typeof simbolo);
+
+//Tipo clase es una function
+class Person{
+    constructor(name, lastN){
+        this.name=Willy;
+        this.lastN=Wonka;
+    }
+}
+console.log(typeof Person);
+
+//Tipo undefined
+var x;
+console.log(typeof x);
+
+//null = ausencia de valor
+var y = null;
+console.log (typeof y);
